d7e42dc6253708a56d89d75a0643de104b938844
VikasVk03 <vishwakarmavikas0102@gmail.com>
2025-04-18 19:15:41 +0530

    tailwindcss configure

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 import daisyui from "daisyui";
-
+import tailwindcss from "@tailwindcss/vite";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: tailwindcss()[daisyui],
   daisyui: {
     themes: [
       "light",
